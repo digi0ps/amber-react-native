d36282a1eb21d2a4c3d4600083134d36fee340f5
digi0ps <sriru1998@gmail.com>
2019-03-28 01:18:45 +0530

    Add src, few dependencies and react-native-maps.

--- index.js
+++ index.js
@@ -2,8 +2,9 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
+import { AppRegistry } from "react-native";
+// import App from './App';
+import Main from "./src/main.js";
+import { name as appName } from "./app.json";
 
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent(appName, () => Main);
