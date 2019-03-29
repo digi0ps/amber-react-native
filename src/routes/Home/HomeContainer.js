57f600a1c71cffe9c73cad1a6878d4ee473259db
digi0ps <sriru1998@gmail.com>
2019-03-30 00:48:29 +0530

    Organize files in Home module.

--- src/routes/Home/HomeContainer.js
+++ src/routes/Home/HomeContainer.js
@@ -1,6 +1,6 @@
 import { connect } from 'react-redux'
-import Home from '../components/Home'
-import * as acts from '../redux/actions'
+import Home from './components/Home'
+import * as acts from './redux/actions'
 
 const mapStateToProps = state => ({
     status: state.home.status,
--- src/routes/scenes.js
+++ src/routes/scenes.js
@@ -1,14 +1,17 @@
-import React from "react";
-import { Actions, Scene } from "react-native-router-flux";
-import HomeContainer from "./Home/container/HomeContainer";
-import TrackDriverContainer from "./TrackDriver/container/TrackDriverContainer";
+import React from 'react'
+import { Actions, Scene } from 'react-native-router-flux'
+import HomeContainer from './Home'
+import TrackDriverContainer from './TrackDriver/container/TrackDriverContainer'
 
 const scenes = Actions.create(
-	<Scene key="root" hideNavBar>
-		<Scene key="home" component={HomeContainer} title="home" initial />
-		<Scene key="trackDriver" component={TrackDriverContainer} title="trackDriver"/>
-	</Scene>
+    <Scene key="root" hideNavBar>
+        <Scene key="home" component={HomeContainer} title="home" initial />
+        <Scene
+            key="trackDriver"
+            component={TrackDriverContainer}
+            title="trackDriver"
+        />
+    </Scene>,
+)
 
-);
-
-export default scenes;
+export default scenes
